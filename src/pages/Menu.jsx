c981c46b8313f9094c1fbf974b679055961ff006
TatsuKenshi <rockinbg@gmail.com>
2024-07-15 23:12:08 +0200

    main menu page gets  addons and desserts menu categories

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -10,8 +10,12 @@ const Menu = ({ t }) => {
   // const { menuItemsShort, sideDishes } = useOutletContext();
   const [menuItems, setMenuItems] = useState([]);
   const [sides, setSides] = useState([]);
+  const [addons, setAddons] = useState([]);
+  const [desserts, setDesserts] = useState([]);
   const menuItemsUrl = process.env.REACT_APP_MENU_ITEMS_URL;
   const sidesUrl = process.env.REACT_APP_SIDES_URL;
+  const addonsUrl = process.env.REACT_APP_ADDONS_URL;
+  const dessertsUrl = process.env.REACT_APP_DESSERTS_URL;
 
   const getAllMenuItems = async () => {
     try {
@@ -33,6 +37,26 @@ const Menu = ({ t }) => {
     }
   };
 
+  const getAllAddonItems = async () => {
+    try {
+      const response = await axios.get(addonsUrl);
+      const data = response.data.addons;
+      setAddons(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAllDessertItems = async () => {
+    try {
+      const response = await axios.get(dessertsUrl);
+      const data = response.data.desserts;
+      setDesserts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const { changeTitle } = usePageTitle();
   const language = i18n.language;
 
@@ -40,6 +64,8 @@ const Menu = ({ t }) => {
     changeTitle({ language: language, enTitle: "Menu", srTitle: "Meni" });
     getAllMenuItems();
     getAllSideItems();
+    getAllAddonItems();
+    getAllDessertItems();
   }, [language, changeTitle]);
 
   return (
@@ -72,6 +98,38 @@ const Menu = ({ t }) => {
 
       <div className="max-w-[1200px] mx-auto mb-8">
         {sides.map((item) => {
+          return <SideDishCard key={item._id} {...item} />;
+        })}
+      </div>
+
+      {/* title div */}
+      <div className="text-center mt-16 mb-4 max-w-[1200px] mx-auto">
+        <h1 className="text-[#9e1918] text-3xl font-extrabold">
+          {t("MeniDodaciNaslov")}
+        </h1>
+        <div className="h-1 w-48 bg-[#a62817] mx-auto mt-1"></div>
+        <p className="mt-4 text-justify">{t("MeniDodaciText")}</p>
+        <p className="mt-4 text-justify">{t("MeniDodaciText2")}</p>
+      </div>
+
+      <div className="max-w-[1200px] mx-auto mb-8">
+        {addons.map((item) => {
+          return <SideDishCard key={item._id} {...item} />;
+        })}
+      </div>
+
+      {/* title div */}
+      <div className="text-center mt-16 mb-4 max-w-[1200px] mx-auto">
+        <h1 className="text-[#9e1918] text-3xl font-extrabold">
+          {t("MeniDesertiNaslov")}
+        </h1>
+        <div className="h-1 w-48 bg-[#a62817] mx-auto mt-1"></div>
+        <p className="mt-4 text-justify">{t("MeniDesertiText")}</p>
+        <p className="mt-4 text-justify">{t("MeniDesertiText2")}</p>
+      </div>
+
+      <div className="max-w-[1200px] mx-auto mb-8">
+        {desserts.map((item) => {
           return <SideDishCard key={item._id} {...item} />;
         })}
       </div>
